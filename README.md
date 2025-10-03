9219e77d872937d59f3b72cf91716a6b7050b9d5
Rory griffin <rorygriffin0209@gmail.com>
2025-10-03 15:03:04 +0100

    Lab 2 working

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Year_3Game
 Work for games engineering module
+
+Each lab is in a new branch to set the labs up you have to:
+
+1. create new directory e.g. Lab2
+2. go to new directory and create new branch: $ git checkout -b Lab2
+3. then add the submodule files to the new branch:  git submodule update --init --recursive
+4. edit the CMakeCache.txt file in C:\Year3\Lab{number}\Year_3Game\out\build\x64-Debug to add the correct folder path. Do find/replace e.g
+	find: e.g. C:\Year3\Lab2
+	replace to: e.g. C:\Year3\Lab3
+5. then open the new folder in visual studio and it should build the new lab project.
--- main.cpp
+++ main.cpp
@@ -1,19 +1,76 @@
 #include <SFML/Graphics.hpp>
 
-int main() {
-    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+const sf::Keyboard::Key controls[4] = {
+		sf::Keyboard::A,
+		sf::Keyboard::Z,
+		sf::Keyboard::Up,
+		sf::Keyboard::Down
+};
 
+
+const sf::Vector2f paddleSize(25.f, 100.f);
+const float ballRadius = 10.f;
+const int gameWidth = 800;
+const int gameHeight = 600;
+const float paddleSpeed = 400.f;
+const float paddleOffsetWall = 10.f;
+const float time_step = 0.017f;
+
+
+sf::CircleShape ball;
+sf::RectangleShape paddles[2];
+
+void init() {
+	for (sf::RectangleShape& p : paddles) {
+		p.setSize(paddleSize);
+		p.setOrigin(paddleSize / 2.f);
+	}
+
+	ball.setRadius(ballRadius);
+	ball.setOrigin(ballRadius, ballRadius);	
+
+	paddles[0].setPosition(paddleOffsetWall + paddleSize.x / 2.f, gameHeight / 2.f);
+	paddles[1].setPosition(gameWidth - paddleOffsetWall - paddleSize.x / 2.f, gameHeight / 2.f);
+
+	ball.setPosition(gameWidth / 2.f, gameHeight / 2.f);
+}
+
+void update(float dt) {
+	// handle paddle movement
+	float direction = 0.0f;
+	if (sf::Keyboard::isKeyPressed(controls[0])) {
+		direction--;
+	}
+	if (sf::Keyboard::isKeyPressed(controls[1])) {
+		direction++;
+	}
+	paddles[0].move(sf::Vector2f(0.f, direction * paddleSpeed * dt));
+}
+
+void render(sf::RenderWindow& window) {
+	// Draw Everything
+	window.draw(paddles[0]);
+	window.draw(paddles[1]);
+	window.draw(ball);
+}
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(gameWidth, gameHeight), "Pong Game");
+    init();
+
+    sf::Clock clock;
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
+            if (event.type == sf::Event::Closed)
                 window.close();
-            }
         }
+
+        float dt = clock.restart().asSeconds();
+        update(dt);
+
         window.clear();
-        window.draw(shape);
+        render(window);
         window.display();
     }
     return 0;
